38097d086e0f148a5b36e0d587787433064bc96e
isabely <isabelcristinayepesb@gmail.com>
2023-02-20 22:52:25 -0500

    loops

---
+++ 09-while-loops.js
@@ -0,0 +1,21 @@
+let password = "tomorrow";
+let userGuess = "";
+
+// while (userGuess !== password) {
+//     userGuess = prompt("Please enter your password");
+// }
+
+// alert("Login Succesful");
+
+// 5 * 4 * 3 * 2 * 1
+
+let factorial = 1;
+let number = 5;
+let original = number;
+
+do {
+    factorial = factorial * number;
+    number--
+} while (number > 0);
+
+console.log(original + " factorial is " + factorial);
